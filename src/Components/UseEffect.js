aaa96185aeec956f7a024d57c8a654fe0f62a38f
Guhan.S <sguhan1257132@gmail.com>
2023-07-21 14:23:54 +0530

    created useEffect component

--- src/Components/UseEffect.js
+++ src/Components/UseEffect.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function UseEffect() {
+  const [resources, setResources] = useState('')
   return (
-    <div>UseEffect</div>
+    <>
+      <div>
+        <button onClick={() => { setResources('Users') }}>USERS</button>
+        <button onClick={() => { setResources('Posts') }}>POST</button>
+        <button onClick={() => { setResources('Comments') }}>COMMENTS</button>
+      </div>
+      <div>
+        <h2>{resources}</h2>
+      </div>
+    </>
   )
 }
 
