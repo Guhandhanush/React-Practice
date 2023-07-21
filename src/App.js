ac1ff936424b9198378e345aee6d1e7966a40ea4
Guhan.S <sguhan1257132@gmail.com>
2023-07-21 14:50:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import ArrayUsestate from './Components/ArrayUsestate';
-import InputUsestate from './Components/InputUsestate';
-import UsestateForm from './Components/UsestateForm';
-import UsestateHooks from './Components/UsestateHooks';
+/* import ArrayUsestate from './Components/ArrayUsestate';
+import InputUsestate from './Components/InputUsestate'; */
+import UseEffect from './Components/UseEffect';
+/* import UsestateForm from './Components/UsestateForm';
+import UsestateHooks from './Components/UsestateHooks'; */
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ArrayUsestate />
+        {/* <ArrayUsestate />
         <UsestateForm />
         <InputUsestate />
-        <UsestateHooks />
+        <UsestateHooks /> */}
+        <UseEffect/>
       </header>
     </div>
   );
