e87b06009daed29ba6ecc7e740585c4e373b4fed
Guhan.S <sguhan1257132@gmail.com>
2023-07-31 15:41:18 +0530

    created folder functional component & files added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import { Provider } from 'react-redux';
+/* import { Provider } from 'react-redux'; */
 import './App.css';
-/* import ArrayUsestate from './Components/ArrayUsestate';
-import InputUsestate from './Components/InputUsestate'; */
-import UseEffect from './Components/UseEffect';
-import store from './Store/Store';
-import Dispatch from './Dispatch/Dispatch';
-/* import UsestateForm from './Components/UsestateForm';
-import UsestateHooks from './Components/UsestateHooks'; */
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <ArrayUsestate />
-        <UsestateForm />
-        <InputUsestate />
-        <UsestateHooks /> 
-        */}
-        <UseEffect />
-        <Provider store={store}>
+       
+       {/*  <Provider store={store}>
           <Dispatch />
-        </Provider>
+        </Provider> */}
       </header>
     </div>
   );
